2e1424e0177013cb648edb2ebf10a2116a7b9b29
claireyip <ynyip2@illinois.edu>
2020-04-29 15:50:57 +0800

    Added step 1 of TicTacToe board algorithm -- GameActivity

--- app/src/main/java/com/example/finaltictactoe/GameActivity.java
+++ app/src/main/java/com/example/finaltictactoe/GameActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
+    ButtonState[][] board = new ButtonState[3][3];
+    boolean player1Turn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+    enum ButtonState {
+        OCCUPIED,
+        PLAYER1,
+        PLAYER2
     }
 }

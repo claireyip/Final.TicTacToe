336abdfae9f96cd340352fad140d679252425455
claireyip <ynyip2@illinois.edu>
2020-04-30 14:14:28 +0800

    Reformatted TicTacToe board, switch players, set each players' color, checkforwin (horizontal and vertical, still need diagonal)

--- app/src/main/java/com/example/finaltictactoe/GameActivity.java
+++ app/src/main/java/com/example/finaltictactoe/GameActivity.java
@@ -2,20 +2,128 @@ package com.example.finaltictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
-    ButtonState[][] board = new ButtonState[3][3];
-    boolean player1Turn = true;
+    private Button[][] buttons = new Button[3][3];
+    /**
+     * button state, determines which buttons have been set
+     */
+    private int[][] buttonState = new int[3][3];
+
+    private boolean player1Turn = true;
+
+    private int player1Points;
+    private int player2Points;
+
+    private TextView player1;
+    private TextView player2;
+
+    private int defaultColor = Color.parseColor("#C6C5C2");
+    private int player1Color = Color.parseColor("#0DEBE4" );
+    private int player2Color = Color.parseColor("#EE9B2A");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        player1 = findViewById(R.id.text_view_p1);
+        player2 = findViewById(R.id.text_view_p2);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                /**
+                 * creates the name of the button to look up
+                 */
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                /**
+                 * putting button in array
+                 */
+                buttons[i][j] = findViewById(resID);
+                /**
+                 * setting listener to the onClick function
+                 */
+                buttons[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    /**
+                     * calling buttonClicked function once button has been clicked
+                     */
+                    public void onClick(View view) {
+                        buttonClicked(view);
+                    }
+                });
+               /**
+                 * setting color of button to default
+                 */
+                buttons[i][j].setBackgroundColor(defaultColor);
+                /**
+                 * setting buttonState to 0, which means players have not clicked on the button
+                 */
+                buttonState[i][j] = 0;
+            }
+        }
     }
-    enum ButtonState {
-        OCCUPIED,
-        PLAYER1,
-        PLAYER2
+    private void buttonClicked (View view) {
+        int b = ((Button) view).getId();
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                /**
+                 * check to see if the button that has been clicked is at coordinates i and j
+                 */
+                if (buttons[i][j].getId() == b) {
+                    x = i;
+                    y = j;
+                    break;
+                }
+
+            }
+        }
+        if (buttonState[x][y] != 0) {
+            return;
+
+        }
+        if (player1Turn) {
+            buttons[x][y].setBackgroundColor(player1Color);
+            buttonState[x][y] = 1;
+        } else {
+            buttons[x][y].setBackgroundColor(player2Color);
+            buttonState[x][y] = 2;
+        }
+        /**
+         * switch players
+         */
+        if (player1Turn) {
+            player1Turn = false;
+        } else {
+            player1Turn = true;
+        }
+        if(chechForWin() == 1) {
+            // player 1 wins and reset
+        } else if (chechForWin() == 2) {
+            //player 2 win and reset
+        }
+    }
+    private int chechForWin() {
+        for (int i = 0; i < buttonState.length; i++) {
+            if (0 != buttonState[i][0] && buttonState[i][0] == buttonState[i][1]
+                    && buttonState[i][0] == buttonState[i][2]) {
+                return buttonState[i][0];
+            }
+        }
+        for (int j = 0; j < buttonState[0].length; j++) {
+            if (0 != buttonState[0][j] && buttonState[0][j] == buttonState[1][j]
+                    && buttonState[0][j] == buttonState[2][j]) {
+                return buttonState[0][j];
+            }
+        }
+        return 0;
     }
 }
